590d31c6bd2dd6e0e2e7372c59a331ab4bf76eeb
Sergey Popkov <100078912+SergeyPazin@users.noreply.github.com>
2024-02-14 16:07:51 +0300

    Add files via upload

---
+++ without RSA/Client_Project.cpp
@@ -0,0 +1,64 @@
+﻿#pragma comment(lib, "ws2_32.lib")
+#include <winsock2.h>
+#include <iostream>
+#include <string>
+#pragma warning(disable: 4996)
+
+SOCKET Connection;
+
+void ClientHandler() {
+	int msg_size;
+	while (true) {
+		recv(Connection, (char*)&msg_size, sizeof(int), NULL);
+		char* msg = new char[msg_size + 1];
+		msg[msg_size] = '\0';
+		recv(Connection, msg, msg_size, NULL);
+		std::cout << msg << std::endl;
+
+		delete[] msg;
+	}
+}
+
+int main(int argc, char* argv[])
+{
+	// WSAStartup
+	WSAData wsaData;
+	WORD DLLVersion = MAKEWORD(2, 1);
+
+	if (WSAStartup(DLLVersion, &wsaData) != 0) {
+		std::cout << "Error" << std::endl;
+		exit(1);
+	}
+
+	SOCKADDR_IN addr;
+	int sizeofaddr = sizeof(addr);
+	addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	addr.sin_port = htons(1111);
+	addr.sin_family = AF_INET;
+
+	Connection = socket(AF_INET, SOCK_STREAM, NULL);
+	if (connect(Connection, (SOCKADDR*)&addr, sizeof(addr)) != 0) {
+		std:: cout << "Error: failed connect to server.\n";
+		return 1;
+	}
+
+	std::cout<< "Connected!\n";
+	/*char msg[256];
+	recv(Connection, msg, sizeof(msg), NULL);
+	std::cout << msg << std::endl;*/
+
+	CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)ClientHandler, NULL, NULL, NULL);
+
+	std::string msg1;
+	while (true) {
+		std::getline(std::cin, msg1);
+		int msg_size = msg1.size();
+		send(Connection, (char*)&msg_size, sizeof(int), NULL);
+		send(Connection, msg1.c_str(), msg_size, NULL);
+		Sleep(10);
+	}
+
+	system("pause");
+	return 0;
+
+}
---
+++ without RSA/Server_Project.cpp
@@ -0,0 +1,76 @@
+﻿#pragma comment(lib, "ws2_32.lib")
+#include <winsock2.h>
+#include <iostream>
+
+#pragma warning(disable: 4996)
+
+SOCKET Connections[100];
+int Counter = 0;
+
+void ClientHandler(int index) {
+	int msg_size;
+	while (true) {
+		recv(Connections[index], (char*)&msg_size, sizeof(int), NULL);
+		char* msg = new char[msg_size + 1];
+		msg[msg_size] = '\0';
+		recv(Connections[index], msg, msg_size, NULL);
+		for (int i = 0; i < Counter; i++) {
+			if (i == index) {
+				continue;
+			}
+
+			send(Connections[i], (char*)&msg_size, sizeof(int), NULL);
+			send(Connections[i], msg, msg_size, NULL);
+		}
+		delete[] msg;
+	}
+}
+
+
+int main(int argc, char* argv[])
+{
+	// WSAStartup
+	WSAData wsaData;
+	WORD DLLVersion = MAKEWORD(2, 1);
+
+	if (WSAStartup(DLLVersion, &wsaData) != 0) {
+		std::cout << "Error" << std::endl;
+		exit(1);
+	}
+
+	SOCKADDR_IN addr;
+	int sizeofaddr = sizeof(addr);
+	addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	addr.sin_port = htons(1111);
+	addr.sin_family = AF_INET;
+
+	SOCKET sListen = socket(AF_INET, SOCK_STREAM, NULL);
+	bind(sListen, (SOCKADDR*)&addr, sizeof(addr));
+	listen(sListen, SOMAXCONN);
+
+	SOCKET newConnection;
+	for (int i = 0; i < 100; i++) {
+		newConnection = accept(sListen, (SOCKADDR*)&addr, &sizeofaddr);
+
+		if (newConnection == 0) {
+			std::cout << "Error#2\n";
+		}
+		else {
+			std::cout << "Client Connected!\n";
+			std::string msg= "Hello dear user.";
+			int msg_size = msg.size();
+			send(newConnection, (char*)&msg_size, sizeof(int), NULL);
+			send(newConnection, msg.c_str(), msg_size, NULL);
+
+			Connections[i] = newConnection;
+			Counter++;
+
+			CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)ClientHandler, (LPVOID)(i), NULL, NULL);
+		}
+	}
+
+	
+
+	system("pause");
+	return 0;
+}
